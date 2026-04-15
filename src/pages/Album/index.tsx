603cf223c3d813911cfdb43f638fd5e6bf934f95
victoriaodemakin <victoriaodemakin@gmail.com>
2026-04-15 10:50:01 +0100

    Use Yarn lockfile; remove package-lock.json

--- src/pages/Album/index.tsx
+++ src/pages/Album/index.tsx
@@ -1,50 +1,40 @@
 import React, { useEffect, useRef } from "react";
 import { ChevronLeft } from "lucide-react";
 import Image from "next/image";
-import Photo0 from "../../../public/assets/images/daddy-1.jpg";
-import Photo1 from "../../../public/assets/images/daddy-wedding.jpg";
-import Photo2 from "../../../public/assets/images/daddy-mummy.jpg";
-import Photo3 from "../../../public/assets/images/daddy--and-child.jpg";
-import Photo4 from "../../../public/assets/images/cool-dad.jpg";
-import Photo5 from "../../../public/assets/images/daddy-bride-2.jpg";
-import Photo7 from "../../../public/assets/images/daddy-sons.jpg";
-import Photo6 from "../../../public/assets/images/daddy-bride-2.jpg";
-import Photo8 from "../../../public/assets/images/daddy-family.jpg";
-import Photo9 from "../../../public/assets/images/daddy-family-2.jpg";
-import Photo10 from "../../../public/assets/images/daddy-family-4.jpg";
-import Photo11 from "../../../public/assets/images/daddy-bride-3.jpg";
-import Photo13 from "../../../public/assets/images/daddy-family-5.jpg";
-import Photo14 from "../../../public/assets/images/daddy-grandpa.jpg";
-import Photo16 from "../../../public/assets/images/daddy-family-7.jpg";
-import Photo17 from "../../../public/assets/images/daddy-in-laws.jpg";
-import Photo19 from "../../../public/assets/images/daddy-mummy-2.jpg";
-import Photo20 from "../../../public/assets/images/daddy-son-wedding.jpg";
-import Photo21 from "../../../public/assets/images/daddy-mummy-3.jpg";
-import Photo22 from "../../../public/assets/images/daddy-mummy-son-1.jpg";
-import Photo23 from "../../../public/assets/images/daddy-sons.jpg";
-import Photo24 from "../../../public/assets/images/daddy-wdding-party.jpg";
-import Photo26 from "../../../public/assets/images/daddy-wedding-3.jpg";
-import Photo27 from "../../../public/assets/images/daddy-wedding-4.jpg";
-import Photo28 from "../../../public/assets/images/Olaseni-Babatunde-Olagbaju-1.jpg";
+import Photo1 from "../../../../public/assets/images/paper-header-pic.jpeg";
+import Photo2 from "../../../../public/assets/images/paper-header-pic.jpeg";
+import Photo3 from "../../../../public/assets/images/paper-header-pic.jpeg";
+import Photo4 from "../../../../public/assets/images/paper-header-pic.jpeg";
+import Photo0 from "../../../../public/assets/images/paper-header-pic.jpeg";
+import Photo6 from "../../../../public/assets/images/paper-header-pic.jpeg";
+import Photo7 from "../../../../public/assets/images/paper-header-pic.jpeg";
+import Photo8 from "../../../../public/assets/images/paper-header-pic.jpeg";
+import Photo9 from "../../../../public/assets/images/paper-header-pic.jpeg";
+import Photo10 from "../../../../public/assets/images/paper-header-pic.jpeg";
+import Photo11 from "../../../../public/assets/images/paper-header-pic.jpeg";
+import Photo12 from "../../../../public/assets/images/paper-header-pic.jpeg";
+import Photo13 from "../../../../public/assets/images/paper-header-pic.jpeg";
+import Photo14 from "../../../../public/assets/images/paper-header-pic.jpeg";
+import Photo15 from "../../../../public/assets/images/paper-header-pic.jpeg";
+import Photo16 from "../../../../public/assets/images/paper-header-pic.jpeg";
+import Photo17 from "../../../../public/assets/images/paper-header-pic.jpeg";
+import Photo18 from "../../../../public/assets/images/paper-header-pic.jpeg";
+import Photo19 from "../../../../public/assets/images/paper-header-pic.jpeg";
+import Photo20 from "../../../../public/assets/images/paper-header-pic.jpeg";
+import Photo21 from "../../../../public/assets/images/paper-header-pic.jpeg";
+import Photo22 from "../../../../public/assets/images/paper-header-pic.jpeg";
+import Photo23 from "../../../../public/assets/images/paper-header-pic.jpeg";
+import Photo24 from "../../../../public/assets/images/paper-header-pic.jpeg";
+import Photo25 from "../../../../public/assets/images/paper-header-pic.jpeg";
+import Photo26 from "../../../../public/assets/images/paper-header-pic.jpeg";
+import Photo27 from "../../../../public/assets/images/paper-header-pic.jpeg";
+import Photo28 from "../../../../public/assets/images/paper-header-pic.jpeg";
+import Photo29 from "../../../../public/assets/images/paper-header-pic.jpeg";
+import Photo30 from "../../../../public/assets/images/paper-header-pic.jpeg";
+import Photo31 from "../../../../public/assets/images/paper-header-pic.jpeg";
+import Photo32 from "../../../../public/assets/images/paper-header-pic.jpeg";
+import Photo33 from "../../../../public/assets/images/paper-header-pic.jpeg";
 
-import Photo29 from "../../../public/assets/images/daddy-silbings.jpg";
-
-import Photo30 from "../../../public/assets/images/Olaseni-Babatunde-Olagbaju-4.jpg";
-
-import Photo33 from "../../../public/assets/images/daddy-brother.jpg";
-import Photo35 from "../../../public/assets/images/daddy-all-silbings.jpg";
-import Photo36 from "../../../public/assets/images/daddy-baby-1.jpg";
-import Photo38 from "../../../public/assets/images/daddy-silbing-2.jpg";
-import Photo39 from "../../../public/assets/images/daddy-silbing-3.jpg";
-import Photo40 from "../../../public/assets/images/daddy-silbing-4.jpg";
-import Photo41 from "../../../public/assets/images/daddy-silbing-5.jpg";
-import Photo42 from "../../../public/assets/images/daddy-silbing-6.jpg";
-import Photo37 from "../../../public/assets/images/daddy-silbing-7.jpg";
-import Photo43 from "../../../public/assets/images/daddy-silbings-8.jpg";
-import Photo44 from "../../../public/assets/images/daddy-silbings.jpg";
-import Photo45 from "../../../public/assets/images/family-daddy.jpg";
-import Photo46 from "../../../public/assets/images/daddy-christmas.jpg";
-import Photo47 from "../../../public/assets/images/daddy-family-8.jpg";
 
 const galleryImages = [
   { id: 0, url: Photo0, alt: "young daddy" },
@@ -52,7 +42,6 @@ const galleryImages = [
   { id: 1, url: Photo1, alt: "Family outdoor moment" },
   { id: 3, url: Photo3, alt: "Indoor family time" },
   { id: 4, url: Photo4, alt: "Sunset family" },
-	  { id: 46, url: Photo46, alt: "Together", objectFit: "contain" },
 
   { id: 8, url: Photo8, alt: "Family in water" },
   { id: 9, url: Photo9, alt: "Indoor family time", objectFit: "contain" },
@@ -63,7 +52,6 @@ const galleryImages = [
   { id: 10, url: Photo10, alt: "Sunset family", objectFit: "contain" },
   { id: 13, url: Photo13, alt: "Family outdoor moment", objectFit: "contain" },
  
-	{ id: 36, url: Photo36, alt: "Family photo", objectFit: "contain" },
 
 	{ id: 14, url: Photo14, alt: "Sunset family" },
     { id: 11, url: Photo11, alt: "Happy moments" },
@@ -71,7 +59,6 @@ const galleryImages = [
   { id: 28, url: Photo28, alt: "Portrait" },
 	
   { id: 16, url: Photo16, alt: "Beach time" },
-	  { id: 45, url: Photo45, alt: "Family photo",objectFit: "contain" },
 
   { id: 17, url: Photo17, alt: "Family moment" },
   { id: 19, url: Photo19, alt: "Together", objectFit: "contain" },
@@ -85,17 +72,7 @@ const galleryImages = [
 	{ id: 29, url: Photo29, alt: "Portrait" },
     { id: 33, url: Photo33, alt: "Together" },
 
-	{ id: 35, url: Photo35, alt: "Wedding celebration" },
-  { id: 37, url: Photo37, alt: "Together" },
-  { id: 38, url: Photo38, alt: "Family gathering" },
-  { id: 39, url: Photo39, alt: "Wedding celebration" },
-  { id: 40, url: Photo40, alt: "Family photo" },
-  { id: 41, url: Photo41, alt: "Together" },
-  { id: 42, url: Photo42, alt: "Family gathering" },
-	  { id: 43, url: Photo43, alt: "Family gathering",objectFit: "contain" },
-			  { id: 45, url: Photo47, alt: "Family gathering",objectFit: "contain" },
 
-  { id: 44, url: Photo44, alt: "Wedding celebration" },
 ];
 
 const GalleryFullPage = ({ onBack }) => {
